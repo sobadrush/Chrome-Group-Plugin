825247ac2073ac331750a287c2673325b3772b19
Roger Lo <sobadrush@icloud.com>
2023-04-05 16:55:10 +0800

    feature：新增 background.js 內容
    1. 監聽 tab clode
    2. 監聽 tab windows close

--- background.js
+++ background.js
@@ -1,1 +1,11 @@
 console.log('background.js called...');
+
+// 監聽 tab close
+chrome.tabs.onRemoved.addListener((tabid, removed) => {
+    console.log('tab closed');
+});
+
+// 監聽 tab windows close
+chrome.windows.onRemoved.addListener((windowid) => {
+    console.log('window closed');
+});
