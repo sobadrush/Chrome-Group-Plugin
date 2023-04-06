7b394a67382ebb304de7efbccddafbd3a40b4383
Roger Lo <sobadrush@icloud.com>
2023-04-06 16:59:23 +0800

    1. 整理 popup.js 2. 增加新增時，檢查是否已有關鍵字邏輯

--- popup.js
+++ popup.js
@@ -4,43 +4,36 @@
  * chrome.tabGroups 官方文件: https://developer.chrome.com/docs/extensions/reference/tabGroups/#type-TabGroup
  * chrome.tabs 官方文件: https://developer.chrome.com/docs/extensions/reference/tabs/
  */
-
-const colorList = ["grey", "blue", "red", "yellow", "green", "pink", "purple", "cyan", "orange"];
-
-// var groupArr = [
-//     { name: 'Youtube', urlKeywords: 'youtube', color: 'pink', tabIds: [] },
-//     { name: 'Instagram', urlKeywords: 'instagram', color: 'yellow', tabIds: [] }
-// ];
-var groupArr = []; // 預設空陣列
-
-let getGroupArr = () => {
-    // 從本地瀏覽器中讀取使用者輸入的資料
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// 從本地瀏覽器中讀取使用者輸入的資料
+const getGroupArr = () => {
     return new Promise((resolve, reject) => {
+        let tmpArr = [];
         chrome.storage.local.get(['myGroupArr'], (result) => {
             console.log(`自 Chrome local storage 讀取資料成功:`, result);
             if(result['myGroupArr']) {
-                groupArr = result['myGroupArr'];
+                tmpArr = result['myGroupArr'];
             } else {
-                groupArr = [];
+                tmpArr = [];
             }
-            resolve("get data done.");
+            resolve(tmpArr);
         });
     });
 }
 
-// IIFE
-(async () => {
-    console.log(' ... IIFE - init ... ');
-    await getGroupArr();
-})();
-
 // 取得 _min ~ _max 之間的亂數
-let genRandomInt = (_min, _max) => {
+const genRandomInt = (_min, _max) => {
     return Math.floor(Math.random() * _max) + _min;
 }
 
 // 存儲資料到 const & chrome
-let doSaveData = (_keywords) => {
+const doSaveData = (_keywords) => {
+
+    if(groupArr.find(xx => xx.urlKeywords === _keywords)){
+        alert(`已有此關鍵字: ${_keywords}`);
+        return;
+    }
+
     let dataToSave = { name: _keywords, urlKeywords: _keywords, color: colorList[genRandomInt(0, colorList.length - 1)], tabIds: [] };
 
     // 儲存到 array
@@ -49,12 +42,14 @@ let doSaveData = (_keywords) => {
     // 將使用者輸入的資料儲存到本地瀏覽器中
     chrome.storage.local.set({ 'myGroupArr': groupArr }, () => {
         console.log(`資料儲存至 Chrome local storage 成功`, groupArr);
+        // 清空輸入框
+        document.querySelector('#keywordsInp').value = '';
+        document.querySelector('#keywordsInp').focus();
     });
 }
 
 // 重置邏輯
-let doReset = (kw) => {
-
+const doReset = (kw) => {
     if(kw) {
         groupArr = groupArr.filter(x => x.urlKeywords === kw)
     }
@@ -71,10 +66,52 @@ let doReset = (kw) => {
     });
 }
 
+// 根據 url 中的關鍵字，將之 group
+const groupTabsByUrlKeywords = async (tabs) => {
+    tabs.forEach((tab) => {
+        for (let group of groupArr) {
+            if (tab.url.includes(group.urlKeywords)) {
+                group.tabIds.push(tab.id);
+
+                // 將使用者輸入的資料儲存到本地瀏覽器中
+                chrome.storage.local.set({ 'myGroupArr': groupArr }, () => {
+                    console.log(`根據關鍵字找分頁，push tabIds 後，儲存至 Chrome local storage 成功`, groupArr);
+                });
+            }
+        }
+    });
+
+    groupArr.forEach((group) => {
+        if (group.tabIds.length > 0) {
+            chrome.tabs.group({ tabIds: group.tabIds }, (groupId) => {
+                chrome.tabGroups.update(groupId, {
+                    title: group.name,
+                    color: group.color,
+                });
+            });
+        }
+    });
+};
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+const colorList = ["grey", "blue", "red", "yellow", "green", "pink", "purple", "cyan", "orange"];
+
+// var groupArr = [
+//     { name: 'Youtube', urlKeywords: 'youtube', color: 'pink', tabIds: [] },
+//     { name: 'Instagram', urlKeywords: 'instagram', color: 'yellow', tabIds: [] }
+// ];
+var groupArr = []; // 預設空陣列
+
+// IIFAE 寫法： Immediately Invoked Async Function Expression
+~async function() {
+    console.log(' ... IIFE - init ... ');
+    groupArr = await getGroupArr();
+    console.log('groupArr = ', groupArr);
+}();
+
 // 綁定「新增」按鈕事件 → 注意：keypress事件無法在 chrome extension 上作用
 ['click', 'keypress'].forEach(ee => {
     let elem = document.querySelector("#addBtn");
-
     switch (ee) {
         case 'click':
             elem.addEventListener(ee, (e) => {
@@ -103,8 +140,6 @@ let doReset = (kw) => {
             });
             break;
     }
-    document.querySelector('#keywordsInp').value = '';
-    document.querySelector('#keywordsInp').focus();
 });
 
 // 執行按鈕
@@ -124,7 +159,7 @@ document.querySelector('#cancelGroupBtn').addEventListener("click", async (e) =>
     e.preventDefault();
     e.stopPropagation();
 
-    await getGroupArr(); // 取得 groupArr
+    groupArr = await getGroupArr(); // 取得 groupArr
     groupArr.forEach(gp => {
         if (gp.tabIds.length > 0) {
             chrome.tabs.ungroup(gp.tabIds, () => {
@@ -150,7 +185,7 @@ document.querySelector("#queryKeywords").addEventListener("click", async (e) => 
     e.stopPropagation();
     document.querySelector("#kwTable > tbody tr")?.remove();
 
-    await getGroupArr(); // 取得 groupArr
+    groupArr = await getGroupArr(); // 取得 groupArr
 
     let trString = groupArr.map((perData, idx) => {
         return `<tr>
@@ -193,30 +228,3 @@ document.querySelector("#queryKeywords").addEventListener("click", async (e) => 
         })
     })
 })
-
-// 根據 url 中的關鍵字，將之 group
-const groupTabsByUrlKeywords = async (tabs) => {
-    tabs.forEach((tab) => {
-        for (let group of groupArr) {
-            if (tab.url.includes(group.urlKeywords)) {
-                group.tabIds.push(tab.id);
-
-                // 將使用者輸入的資料儲存到本地瀏覽器中
-                chrome.storage.local.set({ 'myGroupArr': groupArr }, () => {
-                    console.log(`根據關鍵字找分頁，push tabIds 後，儲存至 Chrome local storage 成功`, groupArr);
-                });
-            }
-        }
-    });
-
-    groupArr.forEach((group) => {
-        if (group.tabIds.length > 0) {
-            chrome.tabs.group({ tabIds: group.tabIds }, (groupId) => {
-                chrome.tabGroups.update(groupId, {
-                    title: group.name,
-                    color: group.color,
-                });
-            });
-        }
-    });
-};
